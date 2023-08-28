66159e5db3f218abd1bf3336c495608d9ac9f8e9
guziyuan <1727622296@qq.com>
2023-08-28 16:03:33 +0800

    update error

--- errs/error.go
+++ errs/error.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // NewErrIndexOutOfRange 创建一个代表下标超出范围的错误
 func NewErrIndexOutOfRange(lenth int, idx int) error {
-	return fmt.Errorf("gtools : 下标超出范围，长度 %d，下标 %d", lenth, idx)
+	return fmt.Errorf("gtools : 下标超出范围，长度 %d, 下标 %d", lenth, idx)
 }
 
 // NewErrEmptySlice 创建一个代表空切片异常的错误
